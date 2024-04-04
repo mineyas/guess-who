5c1525f1195c536b77a6bb499db20b082cd3f261
yasxtech <ykharroubi@xtechcloud.com>
2024-04-04 17:34:10 +0200

    added error message handling

---
+++ guess-who-server/src/utils/messages.js
@@ -0,0 +1,15 @@
+exports.messages = {
+  OK: "OK",
+  CREATED: "Created",
+  BAD_REQUEST: "Bad request",
+  UNAUTHORIZED: "Unauthorized",
+  FORBIDDEN: "Forbidden",
+  NOT_FOUND: "Not found",
+  CONFLICT: "Conflict",
+  INTERNAL_SERVER_ERROR: "Internal server error",
+  ERROR_CREATION: "Error adding element",
+  DELETE_SUCCESS: "Element deleted",
+  DELETE_FAIL: "Error deleting element",
+  UPDATE_SUCCESS: "Element updated",
+  UPDATE_FAIL: "Error updating element",
+};
