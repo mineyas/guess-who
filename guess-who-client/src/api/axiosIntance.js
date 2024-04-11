2865a2768bae8ccfad784577156d4dea3e5eab4e
mineyas <mineyaskha@gmail.com>
2024-04-11 21:00:27 +0200

    refacto axios files

---
+++ guess-who-client/src/api/axiosIntance.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+const handleApiError = (error) => {
+  if (error.response) {
+    console.error("Response data:", error.response.data);
+    console.error("Response status:", error.response.status);
+    console.error("Response headers:", error.response.headers);
+  } else if (error.request) {
+    console.error("No response received:", error.request);
+  } else {
+    console.error("Error during request setup:", error.message);
+  }
+};
+
+export const fetchData = async (endpoint, method, dataForm) => {
+  try {
+    const response = await axiosInstance[method](endpoint, dataForm);
+    return response.data;
+  } catch (error) {
+    handleApiError(error);
+    throw error;
+  }
+};
---
+++ guess-who-client/src/api/routes.js
@@ -0,0 +1,34 @@
+import { fetchData } from "./axiosIntance";
+
+export const signupPost = async (dataForm) =>
+  fetchData("/signup", "post", dataForm);
+
+export const loginPost = async (dataForm) =>
+  fetchData("/login", "post", dataForm);
+export const logoutGet = async () => fetchData("/logout", "get");
+
+export const addCharacter = async (dataForm) =>
+  fetchData("/admin/character", "post", dataForm);
+
+export const addPlayer = async (dataForm) =>
+  fetchData("/player/add", "post", dataForm);
+
+export const loadAllUsers = async () => fetchData("/admin/users", "get");
+export const loadAllCharacters = async () =>
+  fetchData("/admin/characters", "get");
+
+export const getUser = async (id) => fetchData("/user/" + id, "get");
+export const getPlayer = async (id) => fetchData("/player/" + id, "get");
+export const getPlayerByUserId = async (userId) =>
+  fetchData("/player/user/" + userId, "get");
+export const getUserById = async (id) => fetchData("/user/" + id, "get");
+
+export const updateUser = async (id, dataForm) =>
+  fetchData(`/user/edit/${id}`, "post", dataForm);
+export const blockUser = async (id, dataForm) =>
+  fetchData(`/user/block/${id}`, "post", dataForm);
+export const updateCharacter = async (id, dataForm) =>
+  fetchData(`/admin/character/edit/${id}`, "post", dataForm);
+
+export const deleteCharacter = async (id) =>
+  fetchData("/admin/character/delete/" + id, "delete");
